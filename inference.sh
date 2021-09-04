88e0c21a88990585c980935985f6b50053b0d86e
Amardeep Kumar <kumaramardipsingh@gmail.com>
2021-09-04 22:08:20 +0530

    add insference script

---
+++ inference.sh
@@ -0,0 +1,16 @@
+TASK_TYPE = $1
+MODEL_NAME_PATH= $2
+
+pip install transformers
+pip install -r transformers/examples/legacy/seq2seq/requirements.txt
+
+echo "Requirement are installed"
+
+
+python transformers/examples/legacy/seq2seq/run_eval.py \
+    $MODEL_NAME_PATH \
+    Data/$TASK_TYPE/val.source generated_summary.txt \
+    --reference_path Data/$TASK_TYPE/val.target \
+    --score_path inference_score.txt \
+    --bs 1
+
