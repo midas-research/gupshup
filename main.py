a463d22128aab42e92aebae2d9c6c149f8554063
Amardeep Kumar <kumaramardipsingh@gmail.com>
2021-09-10 10:19:48 +0530

    add demo code

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
-
+from os import os
 from run_eval import run_generate
 
 st.write('# GupShup')
 st.write('## Summarizing Open-Domain Code-Switched Conversations')
 task_type= st.sidebar.selectbox(
-    'Task type',["English to English","English to Hindi"]
+    'Task type',["English to English","Hindi to English"]
 )
 model_name= st.sidebar.selectbox(
     'Model',["Pegasus","mBart"]
@@ -34,7 +34,7 @@ x= "fg"
 
 src_file= None
 tar_file= None
-gen_file= None
+gen_file= "generated_summary.txt"
 score_file= None
 
 
@@ -50,29 +50,69 @@ if conv_submit_button:
         src_fp= open(src_file,'w')
         src_fp.write(conv_ip)
         src_fp.close()
-        st.write( "### Summary")
-        x= "conv"
+        # st.write( "### Summary")
+        
+
 
 
 if src_submit_button:
+    if src_ip=="":
+        st.write("### Please enter path to conversastion file")
+    elif os.path.isfile(src_ip)==False:
+        st.write("### Path to conversastion file is invalid")
+
+    else:
+        src_file= src_ip
+        st.write("summarizing conversastion stored in ",src_file)
+        if tar_ip!="" and os.path.isfile(tar_ip):
+            tar_file= tar_ip
+
+            score_file= "scores.txt"
+        else:
+            st.wrie("Target file is not provided or invalid, score will not be calculated")
+
+    # src and tar file chaeck
     st.write(" src and tar")
     st.write(src_ip)
-    st.write(tar_ip, type(tar_ip))
-    if tar_ip!="":
-        gen_file= "generated_summary.txt"
-        score_file= "scores.txt"
-        # st.write("tar is empty")
-    x= "files"
+    st.write(tar_ip)
+
+tt="h2e"
+if task_type=="English to English":
+    tt="e2e"
+elif task_type=="Hindi to English":
+    tt="h2e"
+model_name_path="ad6398/gupshup_"+str(tt)+"_"+str(model_name).lower()
+
+if src_file== None or os.path.isfile(src_file)==False:
+    st.write("### source file is empty or invalid")
+else:
+
+    result= run_generate(
+        verbose=True,
+        model_name_path= model_name_path,
+        src_txt= src_file,
+        tar_txt= tar_file,
+        gen_path=gen_file,
+        scor_path=score_file,
+        batch_size= 8
+    )
+
+    
+
+    if conv_submit_button:
+        st.write("summary:")
+        fp= open(gen_path,'r')
+        summary= fp.readlines()
+        fp.close()
+        st.write(summary)
+
+    if src_submit_button:
+        if tar_file!=None:
+            st.write("scores: ", result)
+        
+        st.write("summary is stored in ", gen_path)
 
 
-run_generate(
-    verbose=True,
-    model_name_path= model_name,
-    src_txt= src_file,
-    tar_txt= tar_file,
-    gen_path=gen_file,
-    scor_path=score_file,
-    batch_size= None
-)
 
-st.write(x)
+
+# st.write(x)
